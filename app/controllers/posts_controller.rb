f4295e293eb7b0d23e238a68f4d1d83d3717830f
daguttt <dagutmu667@gmail.com>
2024-12-12 10:07:06 -0500

    feat: add partial to show form field errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -67,7 +67,7 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:title, :description)
+      params.require(:post).permit(:title, :description, :image)
     end
 
     def post_not_found
